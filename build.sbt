daa94ad860530ed3ec40a71b1042e966529951e0
Mike Kotsur <miccots@gmail.com>
2019-03-10 15:02:46 +0100

    First commit

---
+++ build.sbt
@@ -0,0 +1,35 @@
+name := "elsevier-test"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+resolvers += Resolver.sonatypeRepo("releases")
+
+addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.9")
+addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full)
+
+
+libraryDependencies += "org.typelevel" %% "cats-effect" % "1.2.0" withSources() withJavadoc()
+
+val Http4sVersion = "0.20.0-M6"
+val CirceVersion = "0.11.0"
+
+libraryDependencies += "org.http4s"      %% "http4s-circe"        % Http4sVersion
+libraryDependencies += "org.http4s"      %% "http4s-dsl"          % Http4sVersion
+libraryDependencies += "org.http4s"      %% "http4s-client"       % Http4sVersion
+libraryDependencies += "org.http4s"      %% "http4s-blaze-client" % Http4sVersion
+
+libraryDependencies += "io.circe" %% "circe-generic" % CirceVersion
+libraryDependencies += "io.circe" %% "circe-optics" % CirceVersion
+
+
+
+
+scalacOptions ++= Seq(
+  "-feature",
+  "-deprecation",
+  "-unchecked",
+  "-language:postfixOps",
+  "-language:higherKinds",
+  "-Ypartial-unification")
---
+++ src/main/scala/io/mkotsur/test/App.scala
@@ -0,0 +1,71 @@
+package io.mkotsur.test
+
+import cats.Show
+import cats.effect._
+import org.http4s._
+import org.http4s.client.blaze._
+import org.http4s.client.dsl.io._
+import org.http4s.headers._
+import org.http4s.util.CaseInsensitiveString
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.io.StdIn
+
+object App extends IOApp {
+
+  private def repositoriesUrls(username: String, token: Credentials.Token) = Method.GET(
+    Uri.uri("https://api.github.com/users/") / username / "repos",
+    Authorization(token)
+  )
+
+  private def contributorsInfo(repoUrl: String, token: Credentials.Token): IO[Request[IO]] = for {
+    uri <- IO.fromEither(Uri.fromString(repoUrl))
+    req <- Method.GET(uri, Authorization(token))
+  } yield req
+
+
+  case class Contributor(login: String, contributions: Int)
+  case class RepoInfo(full_name: String, contributors_url: String, fork: Boolean)
+
+  implicit val contributionsShow = new Show[(String, List[Contributor])] {
+    override def show(t: (String, List[Contributor])): String = t match {
+      case (repoName, Nil) => s"$repoName ->  ~~ no contributions ~~ \n\n"
+      case (repoName, contributors) => s"$repoName -> \n" + contributors.mkString("\n") + "\n\n"
+      }
+  }
+
+  override def run(args: List[String]): IO[ExitCode] = {
+
+    import cats.implicits._
+    import io.circe.generic.auto._
+    import org.http4s._
+    import org.http4s.circe._
+
+    implicit val seqContributerDecoder: EntityDecoder[IO, List[Contributor]] = jsonOf[IO, List[Contributor]]
+    implicit val seqRepoInfosDecoder: EntityDecoder[IO, List[RepoInfo]] = jsonOf[IO, List[RepoInfo]]
+
+    BlazeClientBuilder[IO](global).resource.use { client =>
+      for {
+        token <- Option(System.getenv("GH_TOKEN")) match {
+          case None => IO.raiseError(new RuntimeException("Please set GH_TOKEN env variable"))
+          case Some(t) => IO.pure(Credentials.Token(CaseInsensitiveString("token"), t))
+        }
+        _ <- IO(println("Please enter your Github ID ?"))
+        name <- IO(StdIn.readLine())
+        repoInfos <- client.expect[List[RepoInfo]](repositoriesUrls(name, token))
+        contributionsPerRepo <- {
+          repoInfos
+            .filterNot(_.fork)
+            .traverse {
+            case RepoInfo(repoName, contributionsUrl, _) =>
+              client.expect[List[Contributor]](contributorsInfo(contributionsUrl, token)).map { contributors =>
+                contributors.filterNot(_.login == name).sortBy(_.contributions)(Ordering[Int].reverse)
+              } map { (repoName, _) }
+          }
+        }
+        _ <- IO(println(contributionsPerRepo.show))
+        _ <- IO(println(s"Hello, $name"))
+      } yield ExitCode.Success
+    }
+  }
+}
