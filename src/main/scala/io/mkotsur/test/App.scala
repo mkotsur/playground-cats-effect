d92cb8c590eeced4d20a96f505037130fe856b9f
Mike Kotsur <miccots@gmail.com>
2019-03-15 18:35:34 +0100

    Generic pagination

--- src/main/scala/io/mkotsur/test/App.scala
+++ src/main/scala/io/mkotsur/test/App.scala
@@ -1,13 +1,10 @@
 package io.mkotsur.test
 
-import cats.Show.show
-import cats.data.EitherT
 import cats.effect._
 import cats.implicits._
-import cats.{Applicative, Traverse}
 import io.circe.generic.auto._
 import org.http4s._
-import org.http4s.circe.jsonOf
+
 import org.http4s.client.Client
 import org.http4s.client.blaze._
 import org.http4s.client.dsl.io._
@@ -19,47 +16,32 @@ import scala.io.StdIn
 
 object App extends IOApp {
 
+  import Decoders.implicits._
+
   private def readGithubToken(envKey: String) = Option(System.getenv(envKey)) match {
-    case None => IO.raiseError(new RuntimeException(s"Please set $envKey env variable"))
+    case None    => IO.raiseError(new RuntimeException(s"Please set $envKey env variable"))
     case Some(t) => IO.pure(Credentials.Token(CaseInsensitiveString("token"), t))
   }
 
-  private def repositoriesUrls(username: String, token: Credentials.Token) = Method.GET(
-    Uri.uri("https://api.github.com/users/") / username / "repos",
-    Authorization(token)
-  )
-
-  type ContributorsAndNextPage = (List[Contributor], Option[Uri])
-
-  implicit val seqRepoInfosDecoder = jsonOf[IO, List[RepoInfo]]
-
-  implicit val seqContributorDecoder = new EntityDecoder[IO, ContributorsAndNextPage] {
-    private val bodyDecoder = jsonOf[IO, List[Contributor]]
-
-    override def decode(msg: Message[IO], strict: Boolean): DecodeResult[IO, ContributorsAndNextPage] = {
-      val linkHeaderParseResults = msg.headers.filter(_.name === Link.name).flatMap(_.value.split(",")).map(h => Link.parse(h.trim)).toList
-      val linkHeadersEither = Traverse[List].traverse(linkHeaderParseResults)(identity).left.map(pf => new DecodeFailure {
-        override def message: String = pf.getMessage()
-        override def cause: Option[Throwable] = Some(pf)
-        override def toHttpResponse[F[_]](httpVersion: HttpVersion)(implicit F: Applicative[F]): F[Response[F]] = ???
-      })
-
-      for {
-        body <- bodyDecoder.decode(msg, strict = true)
-        links <- EitherT.fromEither[IO](linkHeadersEither)
-      } yield {
-        val uriOption = links.find(_.rel.contains("next")).map(_.uri)
-        (body, uriOption)
-      }
+  private def getContributors(uri: Uri, token: Credentials.Token)(implicit client: Client[IO]): IO[List[Contributor]] =
+    client.expect[(List[Contributor], Option[Uri])](Method.GET(uri, Authorization(token))).flatMap {
+      case (contributors, None) => IO.pure(contributors)
+      case (contributors, Some(nextPageUri)) =>
+        getContributors(nextPageUri, token).map { contribsNext =>
+          contribsNext ::: contributors
+        }
     }
 
-    override def consumes: Set[MediaRange] = Set(MediaRange.`application/*`)
-  }
+  private def getRepoInfosUri(username: String) = IO(Uri.uri("https://api.github.com/users/") / username / "repos")
 
-  private def getContributors(uri: Uri, token: Credentials.Token)(implicit client: Client[IO]): IO[List[Contributor]] = {
-    client.expect[ContributorsAndNextPage](Method.GET(uri, Authorization(token))).flatMap {
-      case (contributors, None) => IO.pure(contributors)
-      case (contributors, Some(nextPageUri)) => getContributors(nextPageUri, token).map { contribsNext => contribsNext ::: contributors}
+  private def getRepoInfos(uri: Uri, token: Credentials.Token)(implicit client: Client[IO]): IO[List[RepoInfo]] = {
+    val req = Method.GET(uri, Authorization(token))
+    client.expect[(List[RepoInfo], Option[Uri])](req).flatMap {
+      case (repoInfos, None) => IO.pure(repoInfos)
+      case (repoInfos, Some(nextPageUri)) =>
+        getRepoInfos(nextPageUri, token).map { next =>
+          next ::: repoInfos
+        }
     }
   }
 
@@ -67,33 +49,30 @@ object App extends IOApp {
 
   case class RepoInfo(full_name: String, contributors_url: String, fork: Boolean)
 
-  implicit val contributionsShow = show[(String, List[Contributor])]({
-    case (repoName, Nil) => s"$repoName ->  ~~ no contributions ~~ \n\n"
-    case (repoName, contributors) => s"$repoName -> \n" + contributors.mkString("\n") + "\n\n"
-  })
-
   override def run(args: List[String]): IO[ExitCode] =
     BlazeClientBuilder[IO](global).resource.use { implicit client =>
+      import Output.implicits._
       for {
-        token <- readGithubToken("GH_TOKEN")
-        _ <- IO(println("Please enter your Github ID ?"))
-        name <- IO(StdIn.readLine())
-        repoInfos <- client.expect[List[RepoInfo]](repositoriesUrls(name, token))
-        _ <- IO(println(s"Found ${repoInfos.length} repos"))
+        token        <- readGithubToken("GH_TOKEN")
+        _            <- IO(println("Please enter your Github ID ?"))
+        username     <- IO(StdIn.readLine())
+        repoInfosUri <- getRepoInfosUri(username)
+        repoInfos    <- getRepoInfos(repoInfosUri, token)
+        _            <- IO(println(s"Found ${repoInfos.length} repos"))
         contributorsPerRepo <- repoInfos
-          .filterNot(_.fork)
-          .traverse {
-            case RepoInfo(repoName, contributionsUrl, _) =>
-              for {
-                uri <- IO.fromEither(Uri.fromString(contributionsUrl))
-                contributors <- getContributors(uri, token)
-              } yield {
-                val orderedContributors = contributors
-                  .filterNot(_.login == name)
-                  .sortBy(_.contributions)(Ordering[Int].reverse)
-                (repoName, orderedContributors)
-              }
-          }
+                                .filterNot(_.fork)
+                                .traverse {
+                                  case RepoInfo(repoName, contributionsUrl, _) =>
+                                    for {
+                                      uri          <- IO.fromEither(Uri.fromString(contributionsUrl))
+                                      contributors <- getContributors(uri, token)
+                                    } yield {
+                                      val orderedContributors = contributors
+                                        .filterNot(_.login === username)
+                                        .sortBy(_.contributions)(Ordering[Int].reverse)
+                                      (repoName, orderedContributors)
+                                    }
+                                }
         _ <- IO(println(contributorsPerRepo.show))
       } yield ExitCode.Success
     }
